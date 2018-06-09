3f5f41af9369de3078480d05df18c91a807336e6
FISI-Labo07 <FISI-Labo07@F20LAB301-18E>
2018-06-09 11:29:06 -0500

    primer hola mundo de Zeus

---
+++ HolaMundo01/src/holamundo01/HolaMundo01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo01;
+
+/**
+ *
+ * @author FISI-Labo07
+ */
+public class HolaMundo01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("holamundo01.HolaMundo01.main() Zeus");
+    }
+    
+}
